955ab01520d0ce256ec20f54cd2e967df7a9f5bd
kaiquesousa2005 <kaique.freire@hotmail.com>
2025-01-24 09:09:48 -0300

    E-commerce finalizado

--- src/Firebase/firebaseConfig.js
+++ src/Firebase/firebaseConfig.js
@@ -3,25 +3,15 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAzNmS94M2JU1kTkayYmA6Xl99ALj5R8xc",
-  authDomain: "e-commerce-firmino.firebaseapp.com",
-  projectId: "e-commerce-firmino",
-  storageBucket: "e-commerce-firmino.firebasestorage.app",
-  messagingSenderId: "178534086203",
-  appId: "1:178534086203:web:08ff3b4203681d2be8ce1e",
-  measurementId: "G-5015D5SR2Z",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-// };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
